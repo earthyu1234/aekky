33e06db9b3e80442853c58c76c231ff21c9782b9
chanphot wongtaweesup <chanphot@hotmail.co.th>
2018-03-06 18:15:08 +0700

    Add inventory and change scene

---
+++ After the good man/Assets/HAndddddd.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HAndddddd : MonoBehaviour {
+
+	
+	public void OnTriggerEnter2D() {
+		Destroy(gameObject);
+	}
+
+}
---
+++ After the good man/Assets/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name) {
+		Debug.Log("Level load requseted for:" +name);
+		Application.LoadLevel(name);
+	}
+	
+
+	void Update () {
+		
+	}
+}
