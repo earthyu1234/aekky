975436bb38226417448bf16b11083b5b37b0f015
chanphot wongtaweesup <chanphot@hotmail.co.th>
2018-03-06 22:25:46 +0700

    NEW QUICK TIME

---
+++ After the good man/Assets/CountDownTime.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CountDownTime : MonoBehaviour {
+	public string LevelLoad;
+	public float timer = 10f;
+	public Text timerSeconds;
+
+
+	void Start () 
+	{
+
+		timerSeconds = GetComponent<Text>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		timer -= Time.deltaTime;
+		timerSeconds.text = timer.ToString("f2");
+		if (timer <= 0)
+		{
+		
+		Application.LoadLevel(LevelLoad);
+	  
+		}
+		
+	}
+}
