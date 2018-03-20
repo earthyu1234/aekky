104915171cd7bdcfc0c5207a4dcf626af4b15bdb
AEKKY\USER <earthyu4@gmail.com>
2018-03-21 03:46:01 +0700

    Alpha game

---
+++ Test inventory c copy/Assets/Scripts/Levelmaneger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Levelmaneger : MonoBehaviour {
+    public void LoadLevel(string name)
+    {
+        Debug.Log("Level load requseted for:" + name);
+        Application.LoadLevel(name);
+    }
+
+    
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
