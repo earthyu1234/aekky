242282550a492652707fd5dea3cafe8f0fa34c73
chanphot wongtaweesup <chanphot@hotmail.co.th>
2018-04-03 16:17:54 +0700

    Zoom object

---
+++ Test Zoom /Assets/Scripts/imageup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class imageup : MonoBehaviour {
+
+	public Image image;
+	void Start () {
+
+		image.enabled = false;
+		
+	}
+	
+	// Update is called once per frame
+	void OnMouseDown () {
+
+		image.enabled = !image.enabled;
+	}
+}
