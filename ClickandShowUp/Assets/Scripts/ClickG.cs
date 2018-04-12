2f3761131e647871a4f33ed3b6d81f82974006cc
chanphot wongtaweesup <chanphot@hotmail.co.th>
2018-04-12 23:59:11 +0700

    Add the show up when click object another object will apper

---
+++ ClickandShowUp/Assets/Scripts/ClickG.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ClickG : MonoBehaviour {
+
+	public static string nameofobj;
+	public GameObject Next;
+
+	// Use this for initialization
+	void Start () {
+		Next.active = false;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown()
+
+	{
+
+	    nameofobj = gameObject.name;
+		Destroy (gameObject);
+		Next.active = !Next.active;
+
+
+		
+
+	}
+}
+
