87f7221badb1fa62196d123847f94e0e948eef7c
chanphot wongtaweesup <chanphot@hotmail.co.th>
2018-04-15 02:21:32 +0700

    Develop scene and unlock

---
+++ Develop 1/Assets/Scripts/Gone.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Gone : MonoBehaviour {
+
+	public static string nameofobj;
+
+
+	// Use this for initialization
+	void Start () {
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown()
+
+	{
+
+	    nameofobj = gameObject.name;
+		Destroy (gameObject);
+
+
+
+		
+
+	}
+}
