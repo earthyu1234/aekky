f7117e4155d8ac23451da82513af79a79a2b6ae6
AEKKY\USER <earthyu4@gmail.com>
2018-04-18 01:22:12 +0700

    re up

---
+++ ClickandShowUp/Assets/Scripts/ImUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImUp : MonoBehaviour {
+
+	public Image image;
+
+
+
+	public Color loadToColor = Color.white;
+	
+
+	 
+       
+	public void Start () {
+			
+			image.enabled = false;	
+		}
+
+	
+	// Update is called once per frame
+	void OnMouseDown () {
+
+		image.enabled = !image.enabled;
+	}
+}
---
+++ Develop 1/Assets/Scripts/FadeS.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FadeS : MonoBehaviour {
+
+
+	public Color loadToColor = Color.white;
+
+	public string scene;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown()
+
+	{
+		Initiate.Fade(scene,loadToColor,0.5f);	
+
+			
+
+	}
+}
